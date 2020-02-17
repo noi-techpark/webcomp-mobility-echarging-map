429b885252df2d83302495551ed14bdcc212017c
Peter Moser <p.moser@noi.bz.it>
2020-02-17 10:03:01 +0100

    Allow only our 3 main languages

--- src/translations.js
+++ src/translations.js
@@ -2,361 +2,181 @@ export const t = {
   my_position: {
     en: 'My position',
     de: 'Meine Position',
-    it: 'La mia posizione',
-    nl: 'My position',
-    cs: 'My position',
-    pl: 'My position',
-    fr: 'My position',
-    ru: 'My position'
+    it: 'La mia posizione'
   },
   apply_filters: {
     en: 'Apply filters',
     de: 'Filter anwenden',
-    it: 'Applica filtri',
-    nl: 'Apply filters',
-    cs: 'Apply filters',
-    pl: 'Apply filters',
-    fr: 'Apply filters',
-    ru: 'Apply filters'
+    it: 'Applica filtri'
   },
   reset_filters: {
     en: 'Reset filters',
     de: 'Filter zurücksetzen',
-    it: 'Annulla filtri',
-    nl: 'Reset filters',
-    cs: 'Reset filters',
-    pl: 'Reset filters',
-    fr: 'Reset filters',
-    ru: 'Reset filters'
+    it: 'Annulla filtri'
   },
   no_one: {
     en: 'No one',
     de: 'Keiner',
-    it: 'Nessuno',
-    nl: 'No one',
-    cs: 'No one',
-    pl: 'No one',
-    fr: 'No one',
-    ru: 'No one'
+    it: 'Nessuno'
   },
   at_least_one_free_column: {
     en: 'At least one free charging station',
     de: 'Mindestens eine freie Ladesäule',
-    it: 'Almeno una colonnina libera',
-    nl: 'At least one free charging station',
-    cs: 'At least one free charging station',
-    pl: 'At least one free charging station',
-    fr: 'At least one free charging station',
-    ru: 'At least one free charging station'
+    it: 'Almeno una colonnina libera'
   },
   completely_free: {
     en: 'Completely free',
     de: 'Absolut frei',
-    it: 'Completamente libera',
-    nl: 'Completely free',
-    cs: 'Completely free',
-    pl: 'Completely free',
-    fr: 'Completely free',
-    ru: 'Completely free'
+    it: 'Completamente libera'
   },
   search_on_greenmobility: {
     en: 'Search on GreenMobility',
     de: 'Suche nach GreenMobility',
-    it: 'Cerca su GreenMobility',
-    nl: 'Search on GreenMobility',
-    cs: 'Search on GreenMobility',
-    pl: 'Search on GreenMobility',
-    fr: 'Search on GreenMobility',
-    ru: 'Search on GreenMobility'
+    it: 'Cerca su GreenMobility'
   },
   available: {
     en: 'Available',
     de: 'Verfügbar',
-    it: 'Disponibile',
-    nl: 'Available',
-    cs: 'Available',
-    pl: 'Available',
-    fr: 'Available',
-    ru: 'Available'
+    it: 'Disponibile'
   },
   maintenance: {
     en: 'Maintenance',
     de: 'Wartung',
-    it: 'Manutenzione',
-    nl: 'Maintenance',
-    cs: 'Maintenance',
-    pl: 'Maintenance',
-    fr: 'Maintenance',
-    ru: 'Maintenance'
+    it: 'Manutenzione'
   },
   not_received: {
     en: 'Not rec.',
     de: 'Nicht erh.',
-    it: 'Non perv.',
-    nl: 'Not rec.',
-    cs: 'Not rec.',
-    pl: 'Not rec.',
-    fr: 'Not rec.',
-    ru: 'Not rec.'
+    it: 'Non perv.'
   },
   not_available: {
     en: 'Not ava.',
     de: 'Nicht verf.',
-    it: 'Non disp.',
-    nl: 'Not ava.',
-    cs: 'Not ava.',
-    pl: 'Not ava.',
-    fr: 'Not ava.',
-    ru: 'Not ava.'
+    it: 'Non disp.'
   },
   privato_with_p_a: {
     en: 'Private with p.a.',
     de: 'Privat mit ö.Z.',
-    it: 'Privato con a.p.',
-    nl: 'Private with p.a.',
-    cs: 'Private with p.a.',
-    pl: 'Private with p.a.',
-    fr: 'Private with p.a.',
-    ru: 'Private with p.a.'
+    it: 'Privato con a.p.'
   },
   public: {
     en: 'Public',
     de: 'Öffentlich',
-    it: 'Pubblico',
-    nl: 'Public',
-    cs: 'Public',
-    pl: 'Public',
-    fr: 'Public',
-    ru: 'Public'
+    it: 'Pubblico'
   },
   private: {
     en: 'Private',
     de: 'Privat',
-    it: 'Privato',
-    nl: 'Private',
-    cs: 'Private',
-    pl: 'Private',
-    fr: 'Private',
-    ru: 'Private'
+    it: 'Privato'
   },
   private_withpublicaccess: {
     en: 'Private with public access',
     de: 'Privat mit öffentlichem Zugang',
-    it: 'Privato con pubblico accesso',
-    nl: 'Private with public access',
-    cs: 'Private with public access',
-    pl: 'Private with public access',
-    fr: 'Private with public access',
-    ru: 'Private with public access'
+    it: 'Privato con pubblico accesso'
   },
   occupied: {
     en: 'Occupied',
     de: 'Besetzt',
-    it: 'Occupato',
-    nl: 'Occupied',
-    cs: 'Occupied',
-    pl: 'Occupied',
-    fr: 'Occupied',
-    ru: 'Occupied'
+    it: 'Occupato'
   },
   write_feedback: {
     en: 'Write feedback',
     de: 'Schreibe Feedback',
-    it: 'Scrivi feedback',
-    nl: 'Write feedback',
-    cs: 'Write feedback',
-    pl: 'Write feedback',
-    fr: 'Write feedback',
-    ru: 'Write feedback'
+    it: 'Scrivi feedback'
   },
   directions: {
     en: 'Directions',
     de: 'Richtungen',
-    it: 'Indicazioni stradali',
-    nl: 'Directions',
-    cs: 'Directions',
-    pl: 'Directions',
-    fr: 'Directions',
-    ru: 'Directions'
+    it: 'Indicazioni stradali'
   },
   informations: {
     en: 'Informations',
     de: 'Informationen',
-    it: 'Informazioni',
-    nl: 'Informations',
-    cs: 'Informations',
-    pl: 'Informations',
-    fr: 'Informations',
-    ru: 'Informations'
+    it: 'Informazioni'
   },
   more_informations: {
     en: 'More informations',
     de: 'Mehr Informationen',
-    it: 'Maggiori informazioni',
-    nl: 'More informations',
-    cs: 'More informations',
-    pl: 'More informations',
-    fr: 'More informations',
-    ru: 'More informations'
+    it: 'Maggiori informazioni'
   },
   hours: {
     en: 'Hours',
     de: 'Stunden',
-    it: 'Orari',
-    nl: 'Hours',
-    cs: 'Hours',
-    pl: 'Hours',
-    fr: 'Hours',
-    ru: 'Hours'
+    it: 'Orari'
   },
   available_columns: {
     en: 'available charging stations',
     de: 'verfügbare Ladestationen',
-    it: 'colonnine disponibili',
-    nl: 'available charging stations',
-    cs: 'available charging stations',
-    pl: 'available charging stations',
-    fr: 'available charging stations',
-    ru: 'available charging stations'
+    it: 'colonnine disponibili'
   },
   type_sockets: {
     en: 'Type sockets',
     de: 'Anschlusstypen',
-    it: 'Tipo prese',
-    nl: 'Type sockets',
-    cs: 'Type sockets',
-    pl: 'Type sockets',
-    fr: 'Type sockets',
-    ru: 'Type sockets'
+    it: 'Tipo prese'
   },
   column: {
     en: 'Charging station',
     de: 'Ladestation',
-    it: 'Colonnina',
-    nl: 'Charging station',
-    cs: 'Charging station',
-    pl: 'Charging station',
-    fr: 'Charging station',
-    ru: 'Charging station'
+    it: 'Colonnina'
   },
   payment: {
     en: 'Payment',
     de: 'Bezahlung',
-    it: 'Pagamento',
-    nl: 'Payment',
-    cs: 'Payment',
-    pl: 'Payment',
-    fr: 'Payment',
-    ru: 'Payment'
+    it: 'Pagamento'
   },
   search_filters: {
     en: 'Search filters',
     de: 'Suchfilter',
-    it: 'Filtri di ricerca',
-    nl: 'Search filters',
-    cs: 'Search filters',
-    pl: 'Search filters',
-    fr: 'Search filters',
-    ru: 'Search filters'
+    it: 'Filtri di ricerca'
   },
   cancel_filters: {
     en: 'Cancel filters',
     de: 'Filter entfernen',
-    it: 'Annulla filtri',
-    nl: 'Cancel filters',
-    cs: 'Cancel filters',
-    pl: 'Cancel filters',
-    fr: 'Cancel filters',
-    ru: 'Cancel filters'
+    it: 'Annulla filtri'
   },
   research_range: {
     en: 'Research range',
     de: 'Suchradius',
-    it: 'Raggio di ricerca',
-    nl: 'Research range',
-    cs: 'Research range',
-    pl: 'Research range',
-    fr: 'Research range',
-    ru: 'Research range'
+    it: 'Raggio di ricerca'
   },
   type_of_access: {
     en: 'Type of access',
     de: 'Zugangsart',
-    it: 'Tipo di accesso',
-    nl: 'Type of access',
-    cs: 'Type of access',
-    pl: 'Type of access',
-    fr: 'Type of access',
-    ru: 'Type of access'
+    it: 'Tipo di accesso'
   },
   column_state: {
     en: 'Charging station state',
     de: 'Status der Ladestation',
-    it: 'Stato colonnina',
-    nl: 'Charging station state',
-    cs: 'Charging station state',
-    pl: 'Charging station state',
-    fr: 'Charging station state',
-    ru: 'Charging station state'
+    it: 'Stato colonnina'
   },
   plug_type: {
     en: 'Plug type',
     de: 'Anschlusstyp',
-    it: 'Tipologia presa',
-    nl: 'Plug type',
-    cs: 'Plug type',
-    pl: 'Plug type',
-    fr: 'Plug type',
-    ru: 'Plug type'
+    it: 'Tipologia presa'
   },
   provider: {
     en: 'Provider',
     de: 'Anbieter',
-    it: 'Fornitore',
-    nl: 'Provider',
-    cs: 'Provider',
-    pl: 'Provider',
-    fr: 'Provider',
-    ru: 'Provider'
+    it: 'Fornitore'
   },
   maxPower: {
     en: 'Max power',
     de: 'Maximale Leistung',
-    it: 'Massima potenza erogata',
-    nl: 'Max power',
-    cs: 'Max power',
-    pl: 'Max power',
-    fr: 'Max power',
-    ru: 'Max power'
+    it: 'Massima potenza erogata'
   },
   open_charging_station: {
     en: 'Charging station open',
     de: 'Ladestation offen',
-    it: 'Stazione di ricarica aperta',
-    nl: 'Charging station open',
-    cs: 'Charging station open',
-    pl: 'Charging station open',
-    fr: 'Charging station open',
-    ru: 'Charging station open'
+    it: 'Stazione di ricarica aperta'
   },
   near_places: {
     en: 'Near places',
     de: 'Umgebung',
-    it: 'Nelle vicinanze',
-    nl: 'Near places',
-    cs: 'Near places',
-    pl: 'Near places',
-    fr: 'Near places',
-    ru: 'Near places'
+    it: 'Nelle vicinanze'
   },
   empty_set__nominatim_locations: {
     en: 'No results',
     de: 'Keine Ergebnisse',
-    it: 'Nessun risultato',
-    nl: 'No results',
-    cs: 'No results',
-    pl: 'No results',
-    fr: 'No results',
-    ru: 'No results'
+    it: 'Nessun risultato'
   }
 };

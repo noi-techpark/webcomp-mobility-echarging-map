4206d0384c4dc3c883df4bef6be88190d0e6c78f
Peter Moser <p.moser@noi.bz.it>
2020-02-17 10:11:56 +0100

    Update URL to new Ninja

--- src/api/integreen-life.js
+++ src/api/integreen-life.js
@@ -2,21 +2,16 @@ import { fetch_options } from './constants';
 
 const BASE_PATH = 'https://ipchannels.integreen-life.bz.it/emobility/rest/';
 const BASE_PATH_PLUGS = 'https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/';
-const NINJA_BASE_PATH = 'https://ipchannels.integreen-life.bz.it/ninja/api/v2';
+const NINJA_BASE_PATH = 'https://mobility.api.opendatahub.bz.it/v2/api';
 
 export async function request__get_stations_details() {
   this.is_loading = true;
-  // const response = await fetch(`${BASE_PATH}get-station-details`, fetch_options);
   const request = await fetch(
     `${NINJA_BASE_PATH}/flat/EChargingStation?limit=-1&offset=0&where=sactive.eq.true&shownull=false&distinct=true`,
     fetch_options
   );
-  // this.all_stations_details = await response.json();
   const response = await request.json();
   this.all_stations_details = response.data;
-  // const rresponse = await request.json();
-  // console.log(this.all_stations_details, rresponse.data);
-
   this.is_loading = false;
 }
 

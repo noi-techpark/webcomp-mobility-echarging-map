3e5d58136c601e95ab23477d706dc41675118004
henri <henri@moosbauer.com>
2023-06-20 15:10:02 +0200

    add action

---
+++ .github/workflows/actions/webcomp-test/index.js
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import core from "@actions/core";
+import github from "@actions/github";
+
+try {
+    const originTestEnabled = core.getInput('originTestEnabled');
+    console.log(`Testing for origin is${originTestEnabled ? " " : " not "}enabled`);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
